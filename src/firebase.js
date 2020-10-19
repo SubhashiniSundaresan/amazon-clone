f6ec4603e5bc988af77eaebe4e5536f651ff86d7
Subhashini Sundaresan <subhashinisundaresan@Subhashinis-MBP.fios-router.home>
2020-10-19 12:35:53 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUwWqYvaG_vqA2HnY80HQX1MLh5SyUWw4",
+  authDomain: "clone-40202.firebaseapp.com",
+  databaseURL: "https://clone-40202.firebaseio.com",
+  projectId: "clone-40202",
+  storageBucket: "clone-40202.appspot.com",
+  messagingSenderId: "290985249640",
+  appId: "1:290985249640:web:6cbc9e23ec70db60e8e4dd",
+  measurementId: "G-XLB6MW3HYL"
+};
